c55e1901b12632c5c776359cc92899ad3788bc6c
Anderson <Anderson@DESKTOP-96OU04G>
2023-05-08 07:19:22 -0300

    Atualização segunda feira

---
+++ Trabalho/src/Tudo/pg1.java
@@ -0,0 +1,43 @@
+package Tudo;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class pg1 {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					pg1 window = new pg1();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public pg1() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
